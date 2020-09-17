2491ce594eee7c4d7198ebfb638c41c611c0188f
rogerblancarte <rogerblancarte@gmail.com>
2020-09-17 17:46:56 -0500

    create a friendship model/table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_222235) do
+ActiveRecord::Schema.define(version: 2020_09_17_224526) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "headings", force: :cascade do |t|
     t.text "content"
