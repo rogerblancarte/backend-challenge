f7ccedfe644cac217c19f0bdb10164337803e4e8
rogerblancarte <rogerblancarte@gmail.com>
2020-09-17 14:30:10 -0500

    basic scaffolding for member and website api controllers

---
+++ app/controllers/api/v1/members_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::MembersController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def member_params
+
+  end
+end
---
+++ app/controllers/api/v1/websites_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::WebsitesController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def website_params
+
+  end
+end
