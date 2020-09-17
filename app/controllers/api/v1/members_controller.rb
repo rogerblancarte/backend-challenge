786ce14bb2ececa025c0e3152fabca5202f9577e
rogerblancarte <rogerblancarte@gmail.com>
2020-09-17 15:52:50 -0500

    setup basic members controller and make those tests green

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,20 +1,27 @@
 class Api::V1::MembersController < ApplicationController
+  before_action :set_member, only: [:show]
 
   def index
-
+    @members = Member.all
+    json_response(@members)
   end
 
   def show
-
+    json_response(@member)
   end
 
   def create
-
+    @member = Member.create!(member_params)
+    json_response(@member, :created)
   end
 
   private
 
   def member_params
+    params.permit(:name)
+  end
 
+  def set_member
+    @member = Member.find(params[:id])
   end
 end
