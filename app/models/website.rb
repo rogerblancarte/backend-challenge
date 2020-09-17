5c5eff6fb9f1ec81d259a3b536f5b3f2ce1f008f
rogerblancarte <rogerblancarte@gmail.com>
2020-09-17 17:35:46 -0500

    create a shortened_url on the website from the original before creation

--- app/models/website.rb
+++ app/models/website.rb
@@ -4,9 +4,17 @@ class Website < ApplicationRecord
   belongs_to :member
   has_many   :headings, dependent: :destroy
 
-  after_create :scrape_headings
+  before_create :shorten_url
+  after_create  :scrape_headings
 
   private
+
+  def shorten_url
+    # Didn't make sense as a service since it isn't
+    # directly modifying anything related to our domain
+    bitly = BitlyUrls.new
+    self.shortened_url = bitly.shorten_link(self.url)
+  end
 
   # send the newly created website object to be scraped
   # for headings and create the associations
